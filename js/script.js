598f6d25531d1bb51f22419ad0bf7625e1b6bb8c
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-14 14:55:39 +0200

    aggiuta else per calcolo punti

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 /*
 Il computer deve generare 16 numeri casuali tra 1 e 100, queste saranno le nostre bombe.
 I numeri delle bombe non possono essere duplicati (i 16 numeri devono essere tutti diversi)
-Il giocatore, deve cercare di non prendere le bombe. Gli chiederemo 100 - 16 volte di scegliere un numero, uno alla volta, sempre compreso tra 1 e 100.
+Il giocatore, deve cercare di non prendere le bombe. Gli chiederemo 100 - 16 volte di scegliere un numero,
+uno alla volta, sempre compreso tra 1 e 100.
 L'utente non può inserire 2 volte lo stesso numero
 Ogni  volta che l'utente sceglie un numero che non è presente tra le bombe, guadagna un punto e poi gli chiediamo un altro numero.
 Se il numero scelto dall'utente è presente tra i numeri bomba, la partita termina.
@@ -27,7 +28,6 @@ Quando la partita termina, comunichiamo all'utente il suo punteggio.
 
 //ARRAY PER CONTENERE LE BOMBE
 var bombsList = [];
-// console.table("numero - " + bombsList);
 
 //ARRAY VUOTO PER CONTENERE I NUMERI INSERITI DALL'UTENTE
 var userChoiceList = [];
@@ -46,16 +46,17 @@ console.table(bombsList);
 
 // CHIEDERE NUMERO ALL'UTENTE
 
-var i = 0;
-while (i < 10){
+var i = 1;
+while ((i < 10) && (!bombsList.includes(userChoice))){
     var userChoice = parseInt(prompt("Inserisci un numero"));
-    while ((userChoice > 100) || (userChoiceList.includes(userChoice))) {
-        alert("Errore numero non valido");
+        while ((userChoice > 100) || (userChoiceList.includes(userChoice))) {
+        alert("Errore, numero non valido");
         userChoice = parseInt(prompt("Inserisci un numero"));
-    }  if (bombsList.includes(userChoice)) {
+    }   if ((bombsList.includes(userChoice))) {
         alert("hai perso");
-        console.log("hai totalizzato" + userChoiceList.length + "punti");
-    }   else {
+        console.log("hai totalizzato " + userChoiceList.length + " punti");
+    }  else {
+        alert("HAI GUADAGNATO " + i + " PUNTI");
         userChoiceList.push(userChoice);
     }
     i++
